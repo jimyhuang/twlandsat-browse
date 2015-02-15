65d2e3189a5f64f49c865678f399549100efe749
Jimmy Huang <jimmy@netivism.com.tw>
2015-02-15 15:43:52 +0800

    Better verify get.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ foreach($dirs as $d){
     $list[$d] = '<a href="./?landsat='.$d.'">'.$date.'</a>';
   }
 }
-$map = $_GET['landsat'] ? $_GET['landsat'] : key($list);
+$map = !empty($_GET['landsat']) && $_GET['landsat'][0] === 'L' ? $_GET['landsat'] : key($list);
 $date = $list[$map];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
