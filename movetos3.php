c46525980f0f01b741fc08794e09d9242dba1799
Jimmy Huang <jimmy@netivism.com.tw>
2015-02-25 02:26:49 +0800

    Add move to s3 script.

--- movetos3.php
+++ movetos3.php
@@ -1,3 +1,21 @@
 <?php
 $sapi = php_sapi_name();
-echo $sapi;
+if($sapi != 'cli') {
+  return;
+}
+
+$base = __DIR__.'/processed/';
+$dirs = scandir($base);
+
+$limit = 3;
+$i = 0;
+foreach($dirs as $d){
+  if($i >= $limit) break;
+  `mkdir -p /mnt/s3/processed/$d`;
+  $dir = $base.$d;
+  if(file_exists($dir.'/final-rgb-pan.TIF')){
+    `cp $dir/*.TIF /mnt/s3/processed/$d/`;
+    `rm -f $dir/*.TIF`;
+    $i++;
+  }
+}
