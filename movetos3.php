20a60f1ce5359c2ca02fb5f4927bde1a6e07407d
Jimmy Huang <jimmy@netivism.com.tw>
2015-02-25 10:25:11 +0800

    Change file detection.

--- movetos3.php
+++ movetos3.php
@@ -13,7 +13,7 @@ foreach($dirs as $d){
   if($i >= $limit) break;
   `mkdir -p /mnt/s3/processed/$d`;
   $dir = $base.$d;
-  if(file_exists($dir.'/final-rgb-pan.TIF')){
+  if(file_exists($dir.'/final-rgb-pan.TIF.bz2')){
     `cp $dir/*.TIF.bz2 /mnt/s3/processed/$d/`;
     `rm -f $dir/*.TIF.bz2`;
     $i++;
