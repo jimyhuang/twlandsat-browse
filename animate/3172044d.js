32c27d409fc6d11f3bfd240c19dfb37f81e85c16
Jimmy Huang <jimmy@netivism.com.tw>
2015-04-09 00:30:03 +0800

    update sample js

--- animate/3172044d.js
+++ animate/3172044d.js
@@ -1,13 +1,14 @@
 {
   "subject":"石門水庫水量演變",
-  "image":"https://farm8.staticflickr.com/7635/16740030020_6e8681f5ef_z.jpg",
+  "image":"https://farm9.staticflickr.com/8709/17001921375_0edbee96e0_z.jpg",
   "rows": [
     {"title":"LC81170432014237LGN00", "date":"2014-08-25", "data":"88.83"},
     {"title":"LC81170432014333LGN00", "date":"2014-11-29", "data":"62.11"},
     {"title":"LC81170432014365LGN00", "date":"2014-12-31", "data":"59.81"},
     {"title":"LC81170432015016LGN00", "date":"2015-01-16", "data":"49.58"},
     {"title":"LC81170432015032LGN00", "date":"2015-02-01", "data":"42.2"},
-    {"title":"LC81170432015048LGN00", "date":"2015-02-17", "data":"34"}
+    {"title":"LC81170432015048LGN00", "date":"2015-02-17", "data":"34"},
+    {"title":"LC81170432015096LGN00", "date":"2015-04-06", "data":"24.79"}
   ],
   "zoom": 13,
   "center": [24.814550111759587,121.2949848175049]
