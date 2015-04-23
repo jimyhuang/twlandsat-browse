a3373b1e221641eeb25f3940e09ef5334a7f290f
Jimmy Huang <jimmy@netivism.com.tw>
2015-04-23 18:16:47 +0200

    Add hof list.

--- bin/hof.php
+++ bin/hof.php
@@ -11,8 +11,25 @@ foreach($line as $l){
   $wall[$name][$landsat] = $landsat;
 }
 
+$token = 'http://nspo.g0v.tw/diff/#{area},{name},{name},7,23.276673210348186,122.684326171875,rgb-rgb';
 foreach($wall as $name => $l){
   foreach($l as $k => $landsat){
+    $rawpath = substr($landsat, 3, 6);
+    $day = substr($landsat, 9, 7);
+    $year = substr($day, 0, 4);
+    $day = substr($day, 4);
+    $date = strtotime($year.'-01-01') + 86400*($day-1);
+    $date = date('Y-m-d', $date);
+    
+    $link = str_replace(array(
+      '{area}',
+      '{name}',
+    ), array(
+      $rawpath,
+      $landsat,
+    ), $token);
+    $wall[$name][$k] = $link;
+  /*
     $dir = "../processed/$landsat/tiles-rgb/12";
     $img = FALSE;
     if(is_dir($dir)){
@@ -33,6 +50,7 @@ foreach($wall as $name => $l){
     if($img){
       $wall[$name][$k] = $img;
     }
+  */
   }
 }
 
