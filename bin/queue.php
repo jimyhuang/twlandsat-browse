1752497d10073176a7174a11c1778a7a0b9ed2ad
Jimmy Huang <jimmy@netivism.com.tw>
2015-04-25 05:13:09 +0200

    Add queue handling

---
+++ bin/queue.php
@@ -0,0 +1,44 @@
+<?php
+$base = '/home/twlandsat/queue';
+$tmp = '/tmp';
+$type = $_POST['type'];
+$name = $_POST['name'];
+$landsat = $_POST['landsat'];
+if(!empty($_POST['action']) && !empty($name) && !empty($type)){
+  switch ($_POST['action']){
+    case 'pending':
+      `cat $base/completed | awk '{print $1}' > $tmp/ctmp`;
+      if($type == 'LT5'){
+        $c = `grep -v -x -f $tmp/ctmp $base/pending | grep LT5`;
+      }
+      if($type == 'LC8'){
+        $c = `grep -v -x -f $tmp/ctmp $base/pending | grep LT5`;
+      }
+      if(!empty($c)){
+        $lines = explode("\n", $c);
+        $line_num = array_rand($lines);
+        $processing = $lines[$line_num];
+        if($processing){
+          unset($lines[$line_num]);
+          $c = implode("\n", $lines);
+          $result = file_put_contents("$base/pending", $c, LOCK_EX);
+          if(!empty($result)){
+            echo $processing;
+            $processing = "$processing $name\n";
+            file_put_contents("$base/processing", $processing, FILE_APPEND);
+          }
+          else{
+            echo '0';
+          }
+        }
+      }
+      break;
+    case 'completed':
+      if(!empty($landsat)){
+        $completed = "$landsat $name\n";
+        file_put_contents("$base/completed", $completed, FILE_APPEND);
+        echo "Completed";
+      }
+      break;
+  }
+}
